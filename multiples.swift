5ec0ba1a186040af2fc01ca4c710e56e54b90ee2
phil-haywood <opahaywood@gmail.com>
2024-03-02 09:40:31 -0500

    second commit

---
+++ multiples.swift
@@ -0,0 +1,15 @@
+
+func sumMultiples() -> Int {
+    var sum = 0
+
+    for i in 0..<1000 {
+        if 3 % == 0 || 5 % == 0 {
+            sum += 1
+        }
+    }
+
+    return sum
+}
+
+let result = sumMultiples()
+print("The sum of the multiples of 3 and 5 between 0 and 1000 is: \(result)")
